4f09d0f31e893f7594cff87c36c3bb0fce13326a
Jonathan <jjhollar@cern.ch>
2026-06-07 18:13:43 +0200

    1st commit

---
+++ collect_systematics.cc
@@ -0,0 +1,218 @@
+#include "TFile.h"
+#include "TProfile.h"
+#include "TGraphErrors.h"
+#include "TGraph2D.h"
+#include "TMatrixD.h"
+
+#include <string>
+
+using namespace std;
+
+//----------------------------------------------------------------------------------------------------
+
+TGraphErrors* MakeDiff(const TProfile *p1, const TProfile *p2)
+{
+	TGraphErrors *g = new TGraphErrors;
+
+	for (int bi = 1; bi <= p1->GetNbinsX(); ++bi)
+	{
+		const double xi = p1->GetBinCenter(bi);
+		const double xi_unc = p1->GetBinWidth(bi) / 2.;
+
+		const double c1 = p1->GetBinContent(bi);
+		const double c1_unc = p1->GetBinError(bi);
+
+		const double c2 = p2->GetBinContent(bi);
+
+		int idx = g->GetN();
+		g->SetPoint(idx, xi, c1 - c2);
+		g->SetPointError(idx, xi_unc, c1_unc);
+	}
+
+	return g;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+TGraphErrors* MakeCombination(const vector<TGraphErrors *> &vp)
+{
+	TGraphErrors *g = new TGraphErrors(*vp[0]);
+
+	for (int i = 0; i <= g->GetN(); ++i)
+	{
+		double s2 = 0.;
+
+		for (unsigned int ci = 0; ci < vp.size(); ++ci)
+		{
+			const double c = vp[ci]->GetY()[i];
+			s2 += c*c;
+		}
+
+		g->GetY()[i] = sqrt(s2);
+	}
+
+	return g;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+TGraph2D* MakeCorrelation(const vector<TGraphErrors *> &vp)
+{
+	TGraph *g_ref = vp[0];
+
+	int dim = g_ref->GetN();
+
+	TMatrixD V(dim, dim);
+	for (const auto c : vp)
+	{
+		for (int i = 0; i < dim; ++i)
+		{
+			for (int j = 0; j < dim; ++j)
+			{
+				const double ci = c->GetY()[i];
+				const double cj = c->GetY()[j];
+
+				V(i, j) += ci * cj;
+			}
+		}
+	}
+
+	TMatrixD C(dim, dim);
+	for (int i = 0; i < dim; ++i)
+	{
+		for (int j = 0; j < dim; ++j)
+		{
+			const double denom = V(i, i) * V(j, j);
+			C(i, j) = (denom > 0.) ? V(i, j) / sqrt(denom) : 0.;
+		}
+	}
+
+	TGraph2D *g2_corr = new TGraph2D();
+	g2_corr->SetTitle(";#xi;#xi");
+	for (int i = 0; i < dim; ++i)
+	{
+		for (int j = 0; j < dim; ++j)
+		{
+			const double xi = g_ref->GetX()[i];
+			const double xj = g_ref->GetX()[j];
+
+			const int idx = g2_corr->GetN();
+			g2_corr->SetPoint(idx, xi, xj, C(i, j));
+		}
+	}
+
+	return g2_corr;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void PrintUsage()
+{
+	printf("USAGE: collect_systematics <period>\n");
+}
+
+//----------------------------------------------------------------------------------------------------
+
+int main(int argc, char **argv)
+{
+	// parse command line
+	if (argc != 2)
+	{
+		PrintUsage();
+		return 1;
+	}
+
+	string period = argv[1];
+
+	// config
+	string dir = "./";
+
+	struct Scenario
+	{
+		string name;
+		string simu_eff, simu_none;
+	};
+
+	vector<Scenario> scenarios = {
+
+	  { "alig-x-sym", "proton_reco_misalignment/misalignment_x_sym_validation.root", "proton_reco_misalignment/misalignment_none_validation.root" },
+		{ "alig-x-asym", "proton_reco_misalignment/misalignment_x_asym_validation.root", "proton_reco_misalignment/misalignment_none_validation.root" },
+		{ "alig-y-sym", "proton_reco_misalignment/misalignment_y_sym_validation.root", "proton_reco_misalignment/misalignment_none_validation.root" },
+                { "alig-y-asym", "proton_reco_misalignment/misalignment_y_asym_validation.root", "proton_reco_misalignment/misalignment_none_validation.root" }
+		,
+
+		{ "opt-Lx", "proton_reco_optics/optics_Lx_1_validation.root", "proton_reco_optics/optics_none_1_validation.root" },
+		{ "opt-Lpx", "proton_reco_optics/optics_Lpx_1_validation.root", "proton_reco_optics/optics_none_1_validation.root" },
+		{ "opt-Lpy", "proton_reco_optics/optics_Lpy_1_validation.root", "proton_reco_optics/optics_none_1_validation.root" },
+		{ "opt-xd", "proton_reco_optics/optics_xd_1_validation.root", "proton_reco_optics/optics_none_1_validation.root" },
+
+	};
+
+	vector<string> elements = {
+	  //		"single rp/2",
+		"single rp/3",
+		"single rp/23",
+		//		"single rp/102",
+		"single rp/103",
+		"single rp/123",
+		"multi rp/0",
+		"multi rp/1",
+	};
+
+	// prepare output
+	TFile *f_out = TFile::Open("all_collect_systematics.root", "recreate");
+
+	// process all elements
+	for (const auto &el : elements)
+	{
+		string dn_el = el;
+		dn_el.replace(dn_el.find('/'), 1, "-");
+
+		printf("* %s\n", dn_el.c_str());
+
+		TDirectory *d_el = f_out->mkdir(dn_el.c_str());
+
+		vector<TGraphErrors *> graphs;
+
+		for (const auto &sc : scenarios)
+		{
+			TFile *f_in_eff = TFile::Open((dir + sc.simu_eff).c_str());
+			TFile *f_in_none = TFile::Open((dir + sc.simu_none).c_str());
+
+			if (!f_in_eff || !f_in_none)
+			{
+				printf("ERROR: cannot open input files\n");
+				continue;
+			}
+
+			TProfile *p_eff = (TProfile *) f_in_eff->Get((el + "/p_de_xi_vs_xi_simu").c_str());
+			TProfile *p_none = (TProfile *) f_in_none->Get((el + "/p_de_xi_vs_xi_simu").c_str());
+
+			if (!p_eff || !p_none)
+			{
+				printf("ERROR: cannot load input objects (scenario %s)\n", sc.name.c_str());
+				continue;
+			}
+
+			gDirectory = d_el;
+
+			TGraphErrors *g_diff = MakeDiff(p_eff, p_none);
+			g_diff->Write(sc.name.c_str());
+
+			graphs.push_back(g_diff);
+		}
+
+		if (!graphs.empty())
+		{
+			TGraphErrors *g_comb = MakeCombination(graphs);
+			g_comb->Write("combined");
+
+			MakeCorrelation(graphs)->Write("g2_correlation");
+		}
+	}
+
+	// clean up
+	delete f_out;
+
+	return 0;
+}
---
+++ systematics_2018_cfg.py
@@ -0,0 +1,204 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run2_2018_cff import *
+from Validation.CTPPS.base_cff import *
+process = cms.Process('CTPPSTest', Run2_2018)
+
+# Uncomment an option for only *one* of these variables below
+ASYST = ""
+OSYST = ""
+RSYST = ""
+
+# Alignment
+#ASYST = "none"
+#ASYST = "symx_mis"
+#ASYST = "asymx_mis"
+#ASYST = "symy_mis"
+#ASYST = "asymy_mis"
+
+# Optics
+#OSYST = "none"
+#OSYST = "Lx"
+#OSYST = "Lpx"
+#OSYST = "Lpy"
+#OSYST = "xd"
+
+# Resolution
+#RSYST = "Level1"
+#RSYST = "Level2"
+#RSYST = "Level3"
+#RSYST = "Level4"
+
+
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('Validation.CTPPS.ctppsLHCInfoPlotter_cfi')
+process.load('Configuration.Generator.randomXiThetaGunProducer_cfi')
+process.load("CondCore.CondDB.CondDB_cfi")
+
+# minimal logger settings
+process.MessageLogger = cms.Service("MessageLogger",
+    statistics = cms.untracked.vstring(),
+    destinations = cms.untracked.vstring('cout'),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING')
+    )
+)
+
+# particle generator
+process.generator.xi_max = 0.25
+process.generator.theta_x_sigma = 60.e-6
+process.generator.theta_y_sigma = 60.e-6
+
+# default source
+process.source = cms.Source("EmptySource",
+    firstRun = cms.untracked.uint32(1),
+)
+
+# For Run 3 2023
+process.CondDB.connect = 'frontier://FrontierProd/CMS_CONDITIONS'
+process.PoolDBESSource = cms.ESSource("PoolDBESSource",
+    process.CondDB,
+    toGet = cms.VPSet(cms.PSet(
+        record = cms.string('CTPPSPixelAnalysisMaskRcd'),
+        tag = cms.string("CTPPSPixelAnalysisMask_Run3_v1_hlt"))
+        ))
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag.globaltag = '150X_mcRun2_asymptotic_v1'
+
+# random seeds
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    sourceSeed = cms.PSet(initialSeed = cms.untracked.uint32(98765)),
+    generator = cms.PSet(initialSeed = cms.untracked.uint32(98766)),
+    beamDivergenceVtxGenerator = cms.PSet(initialSeed = cms.untracked.uint32(3849)),
+    ppsDirectProtonSimulation = cms.PSet(initialSeed = cms.untracked.uint32(4981))
+)
+
+# number of events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(int(1E4))
+)
+
+# systematics
+process.ctppsProtonReconstructionSimulationValidator = cms.EDAnalyzer("CTPPSProtonReconstructionSimulationValidator",
+  tagHepMCBeforeSmearing = cms.InputTag("generator", "unsmeared"),
+  tagHepMCAfterSmearing = cms.InputTag("beamDivergenceVtxGenerator"),
+  tagRecoProtonsSingleRP = cms.InputTag("ctppsProtons", "singleRP"),
+  tagRecoProtonsMultiRP = cms.InputTag("ctppsProtons", "multiRP"),
+
+  lhcInfoLabel = cms.string(""),
+
+  outputFile = cms.string("")
+)
+
+# define (biased) optics for reconstruction
+if OSYST != "":
+    process.ctppsModifiedOpticalFunctionsESSource = cms.ESProducer("CTPPSModifiedOpticalFunctionsESSource",
+                                                                inputOpticsLabel = cms.string(""),
+                                                                outputOpticsLabel = cms.string("modified"),
+                                                                   
+                                                                scenario = cms.string(OSYST),
+                                                                factor = cms.double(1),
+
+                                                                rpId_45_N = cms.uint32(3),
+                                                                rpId_45_F = cms.uint32(23),
+                                                                rpId_56_N = cms.uint32(103),
+                                                                rpId_56_F = cms.uint32(123)
+                                                                )
+
+process.generation = cms.Path(process.generator)
+
+process.validation = cms.Path(
+    process.ctppsProtonReconstructionSimulationValidator
+)
+
+# processing path
+process.schedule = cms.Schedule(
+    process.generation,
+    process.validation
+)
+
+from SimPPS.Configuration.Utils import setupPPSDirectSim
+setupPPSDirectSim(process)
+
+process.ctppsBeamParametersFromLHCInfoESSource.vtxOffsetX45 = 0.
+process.ctppsBeamParametersFromLHCInfoESSource.vtxOffsetY45 = 0.
+process.ctppsBeamParametersFromLHCInfoESSource.vtxOffsetZ45 = 0.
+process.source.numberEventsInLuminosityBlock = process.ctppsCompositeESSource.generateEveryNEvents
+
+if ASYST == "symx_mis":
+    for p in process.ctppsCompositeESSource.periods:
+        p.ctppsRPAlignmentCorrectionsDataXML.MisalignedFiles += cms.vstring("/tmp/jjhollar/TestProtonSystematics/misalignment_x_sym.xml")
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_misalignment/misalignment_x_sym_validation.root"
+if ASYST == "asymx_mis":
+    for p in process.ctppsCompositeESSource.periods:
+        p.ctppsRPAlignmentCorrectionsDataXML.MisalignedFiles += cms.vstring("/tmp/jjhollar/TestProtonSystematics/misalignment_x_asym.xml")
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_misalignment/misalignment_x_asym_validation.root"
+if ASYST == "symy_mis":
+    for p in process.ctppsCompositeESSource.periods:
+        p.ctppsRPAlignmentCorrectionsDataXML.MisalignedFiles += cms.vstring("/tmp/jjhollar/TestProtonSystematics/misalignment_y_sym.xml")
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_misalignment/misalignment_y_sym_validation.root"
+if ASYST == "asymy_mis":
+    for p in process.ctppsCompositeESSource.periods:
+        p.ctppsRPAlignmentCorrectionsDataXML.MisalignedFiles += cms.vstring("/tmp/jjhollar/TestProtonSystematics/misalignment_y_asym.xml")
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_misalignment/misalignment_y_asym_validation.root"
+if ASYST == "none":
+    for p in process.ctppsCompositeESSource.periods:
+        p.ctppsRPAlignmentCorrectionsDataXML.MisalignedFiles += cms.vstring("/tmp/jjhollar/TestProtonSystematics/misalignment_none.xml")
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_misalignment/misalignment_none_validation.root"
+
+if OSYST == "Lx":
+    process.ctppsProtons.opticsLabel = "modified"
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_optics/optics_Lx_1_validation.root"
+if OSYST == "Lpx":
+    process.ctppsProtons.opticsLabel = "modified"
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_optics/optics_Lpx_1_validation.root"
+if OSYST == "Lpy":
+    process.ctppsProtons.opticsLabel = "modified"
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_optics/optics_Lpy_1_validation.root"
+if OSYST == "xd":
+    process.ctppsProtons.opticsLabel = "modified"
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_optics/optics_xd_1_validation.root"
+if OSYST == "none":
+    process.ctppsProtons.opticsLabel = "modified"
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_optics/optics_none_1_validation.root"
+
+if RSYST == "Level1":
+    SetLargeTheta(process)
+    SetLevel1(process)    
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_resolution/resolution_th_Large_level_1_validation.root"
+if RSYST == "Level2":
+    SetLargeTheta(process)
+    SetLevel2(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_resolution/resolution_th_Large_level_2_validation.root"
+if RSYST == "Level3":
+    SetLargeTheta(process)
+    SetLevel3(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_resolution/resolution_th_Large_level_3_validation.root"
+if RSYST == "Level4":
+    SetLargeTheta(process)
+    SetLevel4(process)
+    process.ctppsProtonReconstructionSimulationValidator.outputFile = "proton_reco_resolution/resolution_th_Large_level_4_validation.root"
+
+#print(process.dumpPython())
